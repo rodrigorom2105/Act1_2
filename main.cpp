201eb2ed2dfe503a700de9a26c1bf0728db9bb4b
Rodrigo Romero <rocobros21@gmail.com>
2026-09-01 10:19:37 -0600

    Refactor maze solving functions for improved clarity and consistency

--- main.cpp
+++ main.cpp
@@ -1,194 +1,172 @@
-/*
-Using the "Backtracking" and "Branch and Bound" programming techniques write a
-program that solves a maze in C++. You can work in couples.
-
-The program receives from the standard input two integers M and N, followed by M
-lines of N boolean values (0|1) separated by a space, representing the maze.  A
-1 represents a square in which it is possible to move, a 0 is a square through
-which it is NOT possible to pass.
-
-The origin or start square is always the square (0,0) and the exit or goal is
-always the square (M-1, N-1).
-
-The output of the program is a matrix of Boolean values (0|1) representing the
-path out of the maze. You must first display the solution using the backtracking
-technique, and then using the branch and bound technique.
-
-Your program should be called main.cpp and should compile using the g++ command
-in a Linux environment.
-*/
-
-// #include <algorithm>
 #include <climits>
 #include <iostream>
 #include <queue>
 #include <vector>
 using namespace std;
-// using ll = long long;
 
 struct Node {
-    int i, j, g, bound;
-    bool operator<(const Node &otro) const {
-	return bound > otro.bound;
-    }
+	int i, j, g, bound;
+	bool operator<(const Node &otro) const {
+		return bound > otro.bound;
+	}
 };
 
 int getDistance(int i, int j, int M, int N) {
-    return (M - 1 - i) + (N - 1 - j);
+	return (M - 1 - i) + (N - 1 - j);
 }
 
 void printMatrix(vector<vector<int>> &m) {
-    int M = m.size();
-    int N = m[0].size();
-    for (int i = 0; i < M; i++) {
-	for (int j = 0; j < N; j++) {
-	    if (j)
-		cout << " ";
-	    cout << m[i][j];
+	int M = m.size();
+	int N = m[0].size();
+	for (int i = 0; i < M; i++) {
+		for (int j = 0; j < N; j++) {
+			if (j)
+				cout << " ";
+			cout << m[i][j];
+		}
+		cout << "\n";
 	}
-	cout << "\n";
-    }
 }
 
-bool branchAndBound(vector<vector<int>> &laberinto, vector<vector<int>> &res) {
-    int M = laberinto.size();
-    int N = laberinto[0].size();
+bool branchAndBound(vector<vector<int>> &maze, vector<vector<int>> &solution) {
+	int M = maze.size();
+	int N = maze[0].size();
 
-    if (laberinto[0][0] == 0 || laberinto[M - 1][N - 1] == 0)
-	return false;
+	if (maze[0][0] == 0 || maze[M - 1][N - 1] == 0)
+		return false;
 
-    vector<vector<int>> g(M, vector<int>(N, INT_MAX));
-    vector<vector<pair<int, int>>> father(M, vector<pair<int, int>>(N, {-1, -1}));
+	vector<vector<int>> g(M, vector<int>(N, INT_MAX));
+	vector<vector<pair<int, int>>> father(M, vector<pair<int, int>>(N, {-1, -1}));
 
-    priority_queue<Node> alive;
-    g[0][0] = 0;
-    alive.push({0, 0, 0, getDistance(0, 0, M, N)});
+	priority_queue<Node> alive;
+	g[0][0] = 0;
+	alive.push({0, 0, 0, getDistance(0, 0, M, N)});
 
-    int best = INT_MAX;
+	int best = INT_MAX;
 
-    int di[4] = {1, 0, -1, 0};
-    int dj[4] = {0, 1, 0, -1};
+	int di[4] = {1, 0, -1, 0};
+	int dj[4] = {0, 1, 0, -1};
 
-    while (!alive.empty()) {
-	Node n = alive.top();
-	alive.pop();
+	while (!alive.empty()) {
+		Node n = alive.top();
+		alive.pop();
 
-	if (n.bound >= best)
-	    continue;
+		if (n.bound >= best)
+			continue;
 
-	if (n.g > g[n.i][n.j])
-	    continue;
+		if (n.g > g[n.i][n.j])
+			continue;
 
-	if (n.i == M - 1 && n.j == N - 1) {
-	    best = n.g;
-	    continue;
+		if (n.i == M - 1 && n.j == N - 1) {
+			best = n.g;
+			continue;
+		}
+
+		for (int k = 0; k < 4; k++) {
+			int ni = n.i + di[k];
+			int nj = n.j + dj[k];
+
+			if (ni < 0 || ni >= M || nj < 0 || nj >= N)
+				continue;
+			if (maze[ni][nj] == 0)
+				continue;
+
+			int ng = n.g + 1;
+			int nbound = ng + getDistance(ni, nj, M, N);
+
+			if (nbound < best && ng < g[ni][nj]) {
+				g[ni][nj] = ng;
+				father[ni][nj] = {n.i, n.j};
+				alive.push({ni, nj, ng, nbound});
+			}
+		}
 	}
 
-	for (int k = 0; k < 4; k++) {
-	    int ni = n.i + di[k];
-	    int nj = n.j + dj[k];
+	if (best == INT_MAX)
+		return false;
 
-	    if (ni < 0 || ni >= M || nj < 0 || nj >= N)
-		continue;
-	    if (laberinto[ni][nj] == 0)
-		continue;
-
-	    int ng = n.g + 1;
-	    int nbound = ng + getDistance(ni, nj, M, N);
-
-	    if (nbound < best && ng < g[ni][nj]) {
-		g[ni][nj] = ng;
-		father[ni][nj] = {n.i, n.j};
-		alive.push({ni, nj, ng, nbound});
-	    }
+	int i = M - 1;
+	int j = N - 1;
+	while (i != -1) {
+		solution[i][j] = 1;
+		auto p = father[i][j];
+		i = p.first;
+		j = p.second;
 	}
-    }
-
-    if (best == INT_MAX)
-	return false;
-
-    int i = M - 1;
-    int j = N - 1;
-    while (i != -1) {
-	res[i][j] = 1;
-	auto p = father[i][j];
-	i = p.first;
-	j = p.second;
-    }
-    return true;
+	return true;
 }
 
-bool backtrack(int i, int j, vector<vector<int>> &laberinto, vector<vector<int>> &res) {
-    int M = laberinto.size();
-    int N = laberinto[0].size();
-    if (i < 0 || i >= M || j < 0 || j >= N)
+bool backtrack(int i, int j, vector<vector<int>> &maze, vector<vector<int>> &solution) {
+	int rows = maze.size();
+	int cols = maze[0].size();
+	if (i < 0 || i >= rows || j < 0 || j >= cols)
+		return false;
+
+	if (maze[i][j] == 0)
+		return false;
+
+	if (solution[i][j] == 1)
+		return false;
+
+	solution[i][j] = 1;
+	if ((i == rows - 1) && (j == cols - 1))
+		return true;
+
+	if (backtrack(i + 1, j, maze, solution))
+		return true;
+	if (backtrack(i, j + 1, maze, solution))
+		return true;
+	if (backtrack(i - 1, j, maze, solution))
+		return true;
+	if (backtrack(i, j - 1, maze, solution))
+		return true;
+
+	solution[i][j] = 0;
 	return false;
-
-    if (laberinto[i][j] == 0)
-	return false;
-
-    if (res[i][j] == 1)
-	return false;
-
-    res[i][j] = 1;
-    if ((i == M - 1) && (j == N - 1))
-	return true;
-
-    if (backtrack(i + 1, j, laberinto, res))
-	return true;
-    if (backtrack(i, j + 1, laberinto, res))
-	return true;
-    if (backtrack(i - 1, j, laberinto, res))
-	return true;
-    if (backtrack(i, j - 1, laberinto, res))
-	return true;
-
-    res[i][j] = 0;
-    return false;
 }
 
 int main() {
 
-    ios::sync_with_stdio(false);
+	ios::sync_with_stdio(false);
 
-    cin.tie(nullptr);
+	cin.tie(nullptr);
 
-    int M;
-    int N;
-    cout << "Input: " << endl;
-    cin >> M >> N;
+	int M;
+	int N;
+	cout << "Input: " << endl;
+	cin >> M >> N;
 
-    vector<vector<int>> laberinto(M, vector<int>(N, 0));
+	vector<vector<int>> maze(M, vector<int>(N, 0));
 
-    for (int i = 0; i < M; i++) {
-	for (int j = 0; j < N; j++) {
-	    cin >> laberinto[i][j];
+	for (int i = 0; i < M; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> maze[i][j];
+		}
 	}
-    }
 
-    if (laberinto[0][0] == 0 || laberinto[M - 1][N - 1] == 0) {
-	cout << "Invalid input";
+	if (maze[0][0] == 0 || maze[M - 1][N - 1] == 0) {
+		cout << "Invalid input";
+		return 0;
+	}
+
+	cout << "\nOutput : " << endl;
+
+	vector<vector<int>> solutionBacktracking(M, vector<int>(N, 0));
+
+	if (backtrack(0, 0, maze, solutionBacktracking)) {
+		printMatrix(solutionBacktracking);
+		cout << endl;
+	} else {
+		cout << "No backtrack solution" << endl;
+	}
+
+	vector<vector<int>> solutionBranchAndBound(M, vector<int>(N, 0));
+
+	if (branchAndBound(maze, solutionBranchAndBound)) {
+		printMatrix(solutionBranchAndBound);
+	} else {
+		cout << "No branch and bound solution";
+	}
+
 	return 0;
-    }
-
-    cout << "\nOutput : " << endl;
-
-    vector<vector<int>> solutionBacktracking(M, vector<int>(N, 0));
-
-    if (backtrack(0, 0, laberinto, solutionBacktracking)) {
-	printMatrix(solutionBacktracking);
-	cout << endl;
-    } else {
-	cout << "No backtrack solution" << endl;
-    }
-
-    vector<vector<int>> solutionBranchAndBound(M, vector<int>(N, 0));
-
-    if (branchAndBound(laberinto, solutionBranchAndBound)) {
-	printMatrix(solutionBranchAndBound);
-    } else {
-	cout << "No branch and bound solution";
-    }
-
-    return 0;
 }
